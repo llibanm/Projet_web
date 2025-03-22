8ab5a95bfd7086de2baf467e4881b4843fa88c83
llibanm <libanmohamedali61@gmail.com>
2025-03-22 11:30:55 +0100

    database produit migarted, we can start adding now

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -5,10 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+#[Route('/menu', name: 'app_menu')]
 final class MenuController extends AbstractController
 {
-    #[Route('/menu', name: 'app_menu')]
+    #[Route('', name: '')]
     public function index(): Response
     {
         return $this->render('menu/index.html.twig', [
