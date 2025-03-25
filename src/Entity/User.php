0446e02a1cda9eb34163aee3c125c3c4dbf0dc7c
llibanm <libanmohamedali61@gmail.com>
2025-03-25 14:09:00 +0100

    added pays table in database

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['username'])]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+class   User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
